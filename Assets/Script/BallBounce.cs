da63fdaab1f1f3f9325387da12675f1150f04b05
YohanesAgengHZP <81618432+YohanesAgengHZP@users.noreply.github.com>
2021-10-11 20:51:15 +0700

    problem 3

---
+++ Assets/Script/BallBounce.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBounce : MonoBehaviour
+{
+    [SerializeField]
+    private float minVelocity = 10f;
+    private Rigidbody rb;
+
+    private Vector3 lastFrameVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Bounce(collision.contacts[0].normal);
+    }
+
+    private void Bounce(Vector3 collisionNormal)
+    {
+        var speed = lastFrameVelocity.magnitude;
+        var direction = Vector3.Reflect(lastFrameVelocity.normalized, collisionNormal);
+
+        Debug.Log("Out Direction: " + direction);
+        rb.velocity = direction * Mathf.Max(speed, minVelocity);
+    }
+}
--- Assets/Script/velocity.cs
+++ Assets/Script/velocity.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class velocity : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public float y = 0.0f;
+    public float x = 0.0f;
+
     private void Awake()
     {
+        x = 3.0f;
+        y = 5.0f;
         rb = GetComponent<Rigidbody2D>();
     }
     // Start is called before the first frame update
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(5f, 3f);
+        rb.velocity = new Vector2(x, y);
+
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "wallx")
+        {
+            x = -x;
+        }
+
+            if (collision.gameObject.tag == "wally")
+            {
+                y = -y;
+            }
+    }
+    
+
     void Start()
     {
         
