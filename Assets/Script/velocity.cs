1b47a7f46e95774f10fe95b64e8e5e58f3eb6228
YohanesAgengHZP <81618432+YohanesAgengHZP@users.noreply.github.com>
2021-10-11 19:08:09 +0700

    second commit problem 2

---
+++ Assets/Script/velocity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class velocity : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(5f, 3f);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
